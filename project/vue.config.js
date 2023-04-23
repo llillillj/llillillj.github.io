2976f5f798090f84ad4bfc020526db36ee87f365
llillillj <83072738+llillillj@users.noreply.github.com>
2023-04-23 15:40:09 +0900

    vueのテスト

--- project/vue.config.js
+++ project/vue.config.js
@@ -1,15 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  // outputDir:'../docs', // ファイルの出力先ルート
-  publicPath: process.env.NODE_ENV === 'production'
-  ? '/weather-app/'   //任意
-  : '/',
-  pages: { // 特に今回書く必要はない
-    index: {
-      entry: 'src/main.js',
-      template: 'public/index.html',
-      filename: 'index.html', // webview フォルダからの相対パス
-    },
-  }
+  outputDir:'../docs',
 })
