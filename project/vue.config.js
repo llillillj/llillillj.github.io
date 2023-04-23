17bf95ad154549441a8441ade4e51c89a0568a1d
llillillj <83072738+llillillj@users.noreply.github.com>
2023-04-23 15:04:13 +0900

    vueプロジェクトの導入テスト

--- project/vue.config.js
+++ project/vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  outputDir:'../doc', // ファイルの出力先ルート
+  outputDir:'../docs', // ファイルの出力先ルート
   publicPath: './webview', // index.html などの出力されるファイルに書き込まれる、ルートとなるディレクトリ
   pages: { // 特に今回書く必要はない
     index: {
