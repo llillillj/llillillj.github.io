a2181aaf5e0f5adc125516dae16a6d4758e044d8
llillillj <83072738+llillillj@users.noreply.github.com>
2023-04-23 15:22:21 +0900

    vuejsの動作テスト

--- project/vue.config.js
+++ project/vue.config.js
@@ -2,7 +2,9 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   outputDir:'../docs', // ファイルの出力先ルート
-  publicPath: './webview', // index.html などの出力されるファイルに書き込まれる、ルートとなるディレクトリ
+  publicPath: process.env.NODE_ENV === 'production'
+  ? '/weather-app/'   //任意
+  : '/',
   pages: { // 特に今回書く必要はない
     index: {
       entry: 'src/main.js',
