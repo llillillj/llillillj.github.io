9261e6fd6d153ed12f376dca22e2528f189d6748
llillillillj <83072738+llillillj@users.noreply.github.com>
2023-11-20 17:15:10 +0900

    [FIX]remove score functions

--- akiyuki/src/components/snake/GameDisplay.vue
+++ akiyuki/src/components/snake/GameDisplay.vue
@@ -37,6 +37,7 @@
 export default {
   name: "SnakeGame",
   components: {},
+  emits: ["on-gameover"],
   data() {
     return {
       mapData: [
--- akiyuki/src/views/SnakeGame.vue
+++ akiyuki/src/views/SnakeGame.vue
@@ -17,7 +17,7 @@
         The snake will graw on getting light green items, and get gameover on
         touch to red snake or wall.
       </p>
-      <q-table flat bordered dark hide-bottom class="q-mt-md" :rows="scoreBoard" :columns="columns" />
+      <!-- <q-table flat bordered dark hide-bottom class="q-mt-md" :rows="scoreBoard" :columns="columns" /> -->
     </template>
   </q-page>
 
@@ -36,12 +36,12 @@
 
       <q-card-actions align="right">
         <q-btn color="black" label="Exit" @click="play = false" v-close-popup />
-        <q-btn color="black" label="Register" @click="prompt = true; username = '';" v-close-popup />
+        <!-- <q-btn color="black" label="Register" @click="prompt = true; username = '';" v-close-popup /> -->
       </q-card-actions>
     </q-card>
   </q-dialog>
 
-  <q-dialog v-model="prompt" persistent>
+  <!-- <q-dialog v-model="prompt" persistent>
     <q-card style="min-width: 350px">
       <q-card-section>
         <div class="text-h6">Your name</div>
@@ -56,30 +56,29 @@
         <q-btn color="black" label="Add score" @click="addScore" v-close-popup />
       </q-card-actions>
     </q-card>
-  </q-dialog>
+  </q-dialog> -->
 
 </template>
 
 <script>
 import GameDisplay from "../components/snake/GameDisplay.vue";
 
-import { getFirestore, collection, getDocs, query, addDoc, limit, orderBy, serverTimestamp } from "firebase/firestore";
+// import { getFirestore, collection, getDocs, query, addDoc, limit, orderBy, serverTimestamp } from "firebase/firestore";
 
 import dayjs from 'dayjs'
 import 'dayjs/locale/ja'
 
 dayjs.locale('ja')
 
-const db = getFirestore()
-const colRef = collection(db, "/snakge_game")
-const q = query(colRef, orderBy("score", "desc"), limit(5));
+// const db = getFirestore()
+// const colRef = collection(db, "/snakge_game")
+// const q = query(colRef, orderBy("score", "desc"), limit(5));
 
 export default {
   name: "SnakeGame",
   components: {
     GameDisplay,
   },
-  emits: ["on-gameover"],
   data() {
     return {
       play: false,
@@ -89,50 +88,50 @@ export default {
       score: 0,
       username: "",
 
-      scoreBoard: [
-        { rank: 1, username: "user1", score: 19 },
-        { rank: 2, username: "user2", score: 18 },
-        { rank: 3, username: "user3", score: 17 },
-        { rank: 4, username: "user4", score: 16 },
-        { rank: 5, username: "user5", score: 15 },
-      ],
-      columns: [
-        {
-          name: "rank",
-          required: true,
-          label: "Rank.",
-          align: "center",
-          field: (row) => row.rank,
-          format: (val) => `${val}`,
-          sortable: false,
-        },
-        {
-          name: "username",
-          align: "left",
-          label: "UserName",
-          field: "username",
-          sortable: false,
-        },
-        {
-          name: "score",
-          align: "center",
-          label: "Score",
-          field: "score",
-          sortable: false,
-        },
-        {
-          name: "date",
-          align: "right",
-          label: "Date",
-          field: "date",
-          format: (val) => `${dayjs(val).format('YYYY/MM/DD')}`,
-          sortable: false,
-        },
-      ],
+      // scoreBoard: [
+      //   { rank: 1, username: "user1", score: 19 },
+      //   { rank: 2, username: "user2", score: 18 },
+      //   { rank: 3, username: "user3", score: 17 },
+      //   { rank: 4, username: "user4", score: 16 },
+      //   { rank: 5, username: "user5", score: 15 },
+      // ],
+      // columns: [
+      //   {
+      //     name: "rank",
+      //     required: true,
+      //     label: "Rank.",
+      //     align: "center",
+      //     field: (row) => row.rank,
+      //     format: (val) => `${val}`,
+      //     sortable: false,
+      //   },
+      //   {
+      //     name: "username",
+      //     align: "left",
+      //     label: "UserName",
+      //     field: "username",
+      //     sortable: false,
+      //   },
+      //   {
+      //     name: "score",
+      //     align: "center",
+      //     label: "Score",
+      //     field: "score",
+      //     sortable: false,
+      //   },
+      //   {
+      //     name: "date",
+      //     align: "right",
+      //     label: "Date",
+      //     field: "date",
+      //     format: (val) => `${dayjs(val).format('YYYY/MM/DD')}`,
+      //     sortable: false,
+      //   },
+      // ],
     };
   },
   mounted() {
-    this.getScoreBoard();
+    // this.getScoreBoard();
   },
   methods: {
     onGameover(score) {
@@ -140,25 +139,25 @@ export default {
       this.gameover = true;
     },
 
-    async getScoreBoard() {
-      var cnt = 1
-      this.scoreBoard = []
-      const querySnapshot = await getDocs(q);
-      querySnapshot.forEach((doc) => {
-        const pushItem = {
-          rank: cnt,
-          username: doc.data().username,
-          score: doc.data().score,
-          date: doc.data().date.toDate()
-        }
-        this.scoreBoard.push(pushItem)
-        cnt ++
-      });
-    },
+    // async getScoreBoard() {
+    //   var cnt = 1
+    //   this.scoreBoard = []
+    //   const querySnapshot = await getDocs(q);
+    //   querySnapshot.forEach((doc) => {
+    //     const pushItem = {
+    //       rank: cnt,
+    //       username: doc.data().username,
+    //       score: doc.data().score,
+    //       date: doc.data().date.toDate()
+    //     }
+    //     this.scoreBoard.push(pushItem)
+    //     cnt ++
+    //   });
+    // },
 
-    addScore() {
-      addDoc(colRef, { username: this.username, score: this.score, date: serverTimestamp() })
-    },
+    // addScore() {
+    //   addDoc(colRef, { username: this.username, score: this.score, date: serverTimestamp() })
+    // },
   },
 };
 </script>
