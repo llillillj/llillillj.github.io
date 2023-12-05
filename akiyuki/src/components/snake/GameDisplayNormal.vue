07f37f844b3427420bad58c96cc4e330127909df
llillillj <83072738+llillillj@users.noreply.github.com>
2023-12-05 15:43:02 +0900

    add selection of number of enemy

---
+++ akiyuki/src/components/snake/GameDisplayNormal.vue
@@ -0,0 +1,432 @@
+<template>
+  <div class="content flex flex-center q-pl-xs q-pr-xs q-pt-sm q-pb-sm">
+    <template v-for="(row, y) in mapData" v-bind:key="row">
+      <div class="row">
+        <template v-for="(v, x) in row" v-bind:key="v">
+          <div class="column">
+            <div class="cell_" :class="getClass(x, y, v)"></div>
+          </div>
+        </template>
+      </div>
+    </template>
+  </div>
+  <template v-if="mobile">
+    <div
+      class="content flex flex-center q-pl-xs q-pr-xs q-pt-sm q-pb-sm text-white bg-dark row"
+    >
+      <div class="column">
+        <div class="row">
+          <div class="button-space bg-dark"></div>
+          <q-btn
+            label="↑"
+            @click="direction = '↑'"
+            class="button-space bg-white text-dark"
+          ></q-btn>
+          <div class="button-space bg-dark"></div>
+        </div>
+        <div class="row">
+          <q-btn
+            label="←"
+            @click="direction = '←'"
+            class="button-space bg-white text-dark"
+          ></q-btn>
+          <div class="button-space bg-dark"></div>
+          <q-btn
+            label="→"
+            @click="direction = '→'"
+            class="button-space bg-white text-dark"
+          ></q-btn>
+        </div>
+        <div class="row">
+          <div class="button-space bg-dark"></div>
+          <q-btn
+            label="↓"
+            @click="direction = '↓'"
+            class="button-space bg-white text-dark"
+          ></q-btn>
+          <div class="button-space bg-dark"></div>
+        </div>
+      </div>
+    </div>
+  </template>
+  <div>{{ transcription_ }}</div>
+  <div id="console"></div>
+  <!-- <q-btn label="start" @click="startSpeechToTxt"></q-btn> -->
+</template>
+<script>
+export default {
+  name: "SnakeGame",
+  components: {},
+  emits: ["on-gameover"],
+  props: ["nenemy"],
+  data() {
+    return {
+      mapData: [
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      ],
+
+      cell_Class: ["path_", "block_"],
+      playerHeadState: [2, 4],
+      playerStates: [[2, 4]],
+
+      enemiesHeadState: [
+        [10, 6],
+        [6, 10],
+        [8, 17],
+      ],
+      enemiesStates: [
+        [
+          [10, 6],
+          [9, 6],
+          [8, 6],
+        ],
+        [
+          [6, 10],
+          [6, 9],
+          [5, 9],
+        ],
+        [
+          [8, 17],
+          [9, 17],
+          [10, 17],
+        ],
+      ],
+
+      playerMoveInterval: null,
+      enemyMoveInterval: null,
+      enemyExpandInterval: null,
+
+      food_State: [4, 2],
+      direction: null,
+      zeroIndices: null,
+      gameover: false,
+
+      mobile: false,
+
+    };
+  },
+  mounted() {
+    this.enemiesHeadState = this.enemiesHeadState.slice(0, this.nenemy)
+    this.enemiesStates = this.enemiesStates.slice(0, this.nenemy)
+    window.addEventListener("keydown", this.onKeydown);
+    if (navigator.userAgent.match(/(iPhone|iPad|iPod|Android)/i)) {
+      // スマホ・タブレット（iOS・Android）の場合の処理を記述
+      this.mobile = true;
+    }
+  },
+  methods: {
+    getClass(x, y, v) {
+      if (this.isPlayerHead(x, y)) return "player-head";
+      else if (this.isPlayer(x, y)) return "player";
+      else if (this.isEnemyHead(x, y)) return "enemy-head";
+      else if (this.isEnemy(x, y)) return "enemy";
+      else if (this.isfood_(x, y)) return "food_";
+      else return this.cell_Class[v];
+    },
+
+    isGameStarted() {
+      return this.direction !== null;
+    },
+
+    isPlayerHead(x, y) {
+      return this.playerHeadState[0] == y && this.playerHeadState[1] == x;
+    },
+
+    isEnemyHead(x, y) {
+      for (const i in this.enemiesHeadState) {
+        if (
+          this.enemiesHeadState[i][0] == y &&
+          this.enemiesHeadState[i][1] == x
+        )
+          return true;
+      }
+      return false;
+    },
+
+    isfood_(x, y) {
+      return this.food_State[0] == y && this.food_State[1] == x;
+    },
+
+    isPlayer(x, y) {
+      for (const i in this.playerStates) {
+        const player_state = this.playerStates[i];
+        if (player_state[0] == y && player_state[1] == x) {
+          return true;
+        }
+      }
+      return false;
+    },
+
+    isEnemy(x, y) {
+      for (const i in this.enemiesStates) {
+        for (const j in this.enemiesStates[i]) {
+          const enemy_state = this.enemiesStates[i][j];
+          if (enemy_state[0] == y && enemy_state[1] == x) {
+            return true;
+          }
+        }
+      }
+      return false;
+    },
+
+    onKeydown(event) {
+      switch (event.keyCode) {
+        case 38: // 上矢印キー
+          this.direction = "↑";
+          break;
+        case 40: // 下矢印キー
+          this.direction = "↓";
+          break;
+        case 37: // 左矢印キー
+          this.direction = "←";
+          break;
+        case 39: // 右矢印キー
+          this.direction = "→";
+          break;
+      }
+    },
+    movePlayer() {
+      if (!this.isGameStarted()) return;
+      var tmp_state = JSON.parse(JSON.stringify(this.playerHeadState));
+      switch (this.direction) {
+        case "↑":
+          tmp_state[0]--;
+          break;
+        case "↓":
+          tmp_state[0]++;
+          break;
+        case "←":
+          tmp_state[1]--;
+          break;
+        case "→":
+          tmp_state[1]++;
+          break;
+      }
+      this.handleState(tmp_state);
+    },
+
+    handleState(tmp_state) {
+      const [y, x] = tmp_state;
+      if (this.mapData[y][x] == 1 || this.isEnemy(x, y)) {
+        this.clearAllInterval();
+        this.$emit("on-gameover", this.playerStates.length);
+      } else if (this.food_State[0] == y && this.food_State[1] == x) {
+        const pushItem = JSON.parse(
+          JSON.stringify(this.playerStates.slice(-1)[0])
+        );
+        this.playerStates.push(pushItem);
+        this.handleHeadState(tmp_state);
+        this.handleStates();
+        this.changefood_State();
+      } else {
+        this.handleHeadState(tmp_state);
+        this.handleStates();
+      }
+    },
+    handleHeadState(tmp_state) {
+      this.playerHeadState = tmp_state;
+    },
+    handleStates() {
+      this.playerStates.pop();
+      this.playerStates.unshift(
+        JSON.parse(JSON.stringify(this.playerHeadState))
+      );
+    },
+
+    moveEnemy() {
+      if (!this.isGameStarted()) return;
+      for (const i in this.enemiesHeadState) {
+        var random_idx = Math.floor(Math.random() * 4);
+        var tmp_state = JSON.parse(JSON.stringify(this.enemiesHeadState[i]));
+        switch (random_idx) {
+          case 0:
+            tmp_state[0]--;
+            break;
+          case 1:
+            tmp_state[0]++;
+            break;
+          case 2:
+            tmp_state[1]--;
+            break;
+          case 3:
+            tmp_state[1]++;
+            break;
+        }
+        this.enemyHandleState(tmp_state, i);
+      }
+    },
+    enemyHandleState(tmp_state, i) {
+      const [y, x] = tmp_state;
+      if (this.mapData[y][x] == 1) {
+        return;
+      } else if (this.isPlayer(x, y)) {
+        this.clearAllInterval();
+        this.$emit("on-gameover", this.playerStates.length);
+      } else {
+        this.enemyHandleHeadState(tmp_state, i);
+        this.enemyHandleStates(i);
+      }
+    },
+    enemyHandleHeadState(tmp_state, i) {
+      this.enemiesHeadState[i] = tmp_state;
+    },
+    enemyHandleStates(i) {
+      this.enemiesStates[i].pop();
+      this.enemiesStates[i].unshift(
+        JSON.parse(JSON.stringify(this.enemiesHeadState[i]))
+      );
+    },
+    expandEnemy() {
+      for (const i in this.enemiesHeadState) {
+        const pushItem = JSON.parse(
+          JSON.stringify(this.enemiesStates[i].slice(-1)[0])
+        );
+        this.enemiesStates[i].push(pushItem);
+      }
+    },
+
+    clearAllInterval() {
+      clearInterval(this.playerMoveInterval);
+      clearInterval(this.enemyMoveInterval);
+      clearInterval(this.enemyExpandInterval);
+    },
+
+    changefood_State() {
+      this.food_State = this.getRandomZeroIndex();
+    },
+    to_gameover() {
+      this.gameover = true;
+    },
+    getRandomZeroIndex() {
+      if (this.zeroIndices === null) {
+        this.zeroIndices = this.getZeroIndices();
+      }
+      return this.zeroIndices[
+        Math.floor(Math.random() * this.zeroIndices.length)
+      ];
+    },
+    getZeroIndices() {
+      const array = JSON.parse(JSON.stringify(this.mapData));
+      const zeroIndices = [];
+      for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j < array[i].length; j++) {
+          if (array[i][j] === 0) {
+            zeroIndices.push([i, j]);
+          }
+        }
+      }
+      return zeroIndices;
+    },
+    // startSpeechToTxt() {
+    //   // initialisation of voicereco
+
+    //   window.SpeechRecognition =
+    //     window.SpeechRecognition || window.webkitSpeechRecognition;
+    //   const recognition = new window.SpeechRecognition();
+    //   recognition.lang = this.lang_;
+    //   recognition.interimResults = true;
+
+    //   // event current voice reco word
+    //   recognition.addEventListener("result", (event) => {
+    //     var text = Array.from(event.results)
+    //       .map((result) => result[0])
+    //       .map((result) => result.transcript)
+    //       .join("");
+    //     this.runtimeTranscription_ = text;
+    //   });
+    //   // end of transcription
+    //   recognition.addEventListener("end", () => {
+    //     this.transcription_.push(this.runtimeTranscription_);
+    //     this.runtimeTranscription_ = "";
+    //     recognition.stop();
+    //   });
+
+    //   recognition.start();
+    //   // setTimeout(() => {
+    //   //   recognition.stop()
+    //   // }, this.recognitionInterval)
+
+    // },
+  },
+  watch: {
+    direction(newVal, prevVal) {
+      if (newVal !== null && prevVal === null) {
+        const gameInterval = 250;
+        this.playerMoveInterval = setInterval(this.movePlayer, gameInterval);
+        this.enemyMoveInterval = setInterval(this.moveEnemy, gameInterval);
+        this.enemyExpandInterval = setInterval(this.expandEnemy, 10000);
+      }
+    },
+  },
+};
+</script>
+
+<style>
+.content {
+  background-color: white;
+  width: 340px;
+  border-radius: 8px;
+}
+
+.cell_ {
+  width: 14px;
+  height: 14px;
+  margin: 1px;
+  border-radius: 4px;
+}
+
+.block_ {
+  background-color: black;
+}
+
+.path_ {
+  background-color: rgb(206, 206, 206);
+}
+
+.food_ {
+  background-color: #2a2;
+}
+
+.player {
+  background-color: #55c;
+}
+
+.player-head {
+  background-color: #22a;
+}
+
+.enemy {
+  background-color: #c55;
+}
+
+.enemy-head {
+  background-color: #a22;
+}
+
+.button-space {
+  width: 45px;
+  height: 45px;
+  border-radius: 100%;
+}
+
+.display-flex {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
--- akiyuki/src/views/SnakeGame.vue
+++ akiyuki/src/views/SnakeGame.vue
@@ -2,18 +2,29 @@
   <q-page class="flex flex-center column q-pa-md">
     <p class="text-h3">Snake Game</p>
     <template v-if="play">
-      <div class="content flex flex-center q-pl-xs q-pr-xs q-pt-sm q-pb-sm bg-dark q-gutter-y-md">
-        <GameDisplay @on-gameover="onGameover" />
-        <q-btn color="white" class="text-black" @click="play = false">Quit game</q-btn>
+      <div
+        class="content flex flex-center q-pl-xs q-pr-xs q-pt-sm q-pb-sm bg-dark q-gutter-y-md"
+      >
+        <GameDisplayNormal @on-gameover="onGameover" :nenemy="nenemy" />
+        <q-btn color="white" class="text-black" @click="play = false"
+          >Quit game</q-btn
+        >
       </div>
-    </template>
-    <template v-else-if="audio">
-      <AudioCollection @exit-audio="onExitAudio" />
     </template>
     <template v-else>
       <div class="row q-gutter-x-md">
-        <q-btn color="white" class="text-black" @click="play = true">Start</q-btn>
-        <q-btn color="white" class="text-black" @click="audio = true">Audio AI</q-btn>
+        <q-btn color="white" class="text-black" @click="play = true"
+          >Start</q-btn
+        >
+        <q-input
+          dark
+          v-model.number="nenemy"
+          v-on:update:model-value="clipNEnemy"
+          label="Number of enemy"
+          type="number"
+          filled
+          style="max-width: 200px"
+        />
       </div>
       <p class="q-mt-md">
         This game is called snake game. You can control the blue snake with your
@@ -23,7 +34,6 @@
         The snake will graw on getting light green items, and get gameover on
         touch to red snake or wall.
       </p>
-      <!-- <q-table flat bordered dark hide-bottom class="q-mt-md" :rows="scoreBoard" :columns="columns" /> -->
     </template>
   </q-page>
 
@@ -42,134 +52,46 @@
 
       <q-card-actions align="right">
         <q-btn color="black" label="Exit" @click="play = false" v-close-popup />
-        <!-- <q-btn color="black" label="Register" @click="prompt = true; username = '';" v-close-popup /> -->
       </q-card-actions>
     </q-card>
   </q-dialog>
-
-  <!-- <q-dialog v-model="prompt" persistent>
-    <q-card style="min-width: 350px">
-      <q-card-section>
-        <div class="text-h6">Your name</div>
-      </q-card-section>
-
-      <q-card-section class="q-pt-none">
-        <q-input dense v-model="username" autofocus @keyup.enter="prompt = false" />
-      </q-card-section>
-
-      <q-card-actions align="right" class="text-primary">
-        <q-btn color="black" label="Cancel" v-close-popup />
-        <q-btn color="black" label="Add score" @click="addScore" v-close-popup />
-      </q-card-actions>
-    </q-card>
-  </q-dialog> -->
-
 </template>
 
 <script>
-import GameDisplay from "../components/snake/GameDisplay.vue";
-import AudioCollection from "../components/snake/AudioCollection.vue";
-
-// import { getFirestore, collection, getDocs, query, addDoc, limit, orderBy, serverTimestamp } from "firebase/firestore";
-
-// import dayjs from 'dayjs'
-// import 'dayjs/locale/ja'
-
-// dayjs.locale('ja')
-
-// const db = getFirestore()
-// const colRef = collection(db, "/snakge_game")
-// const q = query(colRef, orderBy("score", "desc"), limit(5));
+import GameDisplayNormal from "../components/snake/GameDisplayNormal.vue";
 
 export default {
   name: "SnakeGame",
   components: {
-    GameDisplay,
-    AudioCollection,
+    GameDisplayNormal,
   },
   data() {
     return {
       play: false,
-      audio: false,
       gameover: false,
       prompt: false,
 
+      nenemy: 1,
+
       score: 0,
       username: "",
-
-      // scoreBoard: [
-      //   { rank: 1, username: "user1", score: 19 },
-      //   { rank: 2, username: "user2", score: 18 },
-      //   { rank: 3, username: "user3", score: 17 },
-      //   { rank: 4, username: "user4", score: 16 },
-      //   { rank: 5, username: "user5", score: 15 },
-      // ],
-      // columns: [
-      //   {
-      //     name: "rank",
-      //     required: true,
-      //     label: "Rank.",
-      //     align: "center",
-      //     field: (row) => row.rank,
-      //     format: (val) => `${val}`,
-      //     sortable: false,
-      //   },
-      //   {
-      //     name: "username",
-      //     align: "left",
-      //     label: "UserName",
-      //     field: "username",
-      //     sortable: false,
-      //   },
-      //   {
-      //     name: "score",
-      //     align: "center",
-      //     label: "Score",
-      //     field: "score",
-      //     sortable: false,
-      //   },
-      //   {
-      //     name: "date",
-      //     align: "right",
-      //     label: "Date",
-      //     field: "date",
-      //     format: (val) => `${dayjs(val).format('YYYY/MM/DD')}`,
-      //     sortable: false,
-      //   },
-      // ],
     };
   },
-  mounted() {
-    // this.getScoreBoard();
-  },
+  mounted() {},
   methods: {
-    onExitAudio() {
-      this.audio = false;
-    },
     onGameover(score) {
       this.score = score;
       this.gameover = true;
     },
-
-    // async getScoreBoard() {
-    //   var cnt = 1
-    //   this.scoreBoard = []
-    //   const querySnapshot = await getDocs(q);
-    //   querySnapshot.forEach((doc) => {
-    //     const pushItem = {
-    //       rank: cnt,
-    //       username: doc.data().username,
-    //       score: doc.data().score,
-    //       date: doc.data().date.toDate()
-    //     }
-    //     this.scoreBoard.push(pushItem)
-    //     cnt ++
-    //   });
-    // },
-
-    // addScore() {
-    //   addDoc(colRef, { username: this.username, score: this.score, date: serverTimestamp() })
-    // },
+    clipNEnemy() {
+      const MAX_N_ENEMY = 3
+      const MIN_N_ENEMY = 1
+      if (this.nenemy > MAX_N_ENEMY) {
+        this.nenemy = MAX_N_ENEMY
+      } else if (this.nenemy < MIN_N_ENEMY) {
+        this.nenemy = MIN_N_ENEMY
+      }
+    }
   },
 };
 </script>
