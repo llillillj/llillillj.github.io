6de96001bf5836b9246d9b613c17d00c0c26aca1
llillillj <83072738+llillillj@users.noreply.github.com>
2024-04-16 16:32:29 +0900

    学年の更新

--- akiyuki/src/views/HomeView.vue
+++ akiyuki/src/views/HomeView.vue
@@ -7,7 +7,7 @@
       </div>
       <div class="text-content text-left q-pa-md" style="overflow-y: initial;">
         <p class="text-h5 q-ml-md"><u>自己紹介</u></p>
-        <p class="text-subtitle1">兵庫県立大学 社会情報科学部 社会情報科学科 B3 香山明之</p>
+        <p class="text-subtitle1">兵庫県立大学 社会情報科学部 社会情報科学科 B4 香山明之</p>
         <p>大学内の部活であるデータ分析研究部に所属．現在は研究演習で川嶋研に配属され，水中ロボットに適用する強化学習の勉強をしています．</p>
         <p>webアプリ開発の勉強や機械学習の勉強を主にしつつ，学祭やOCなどの大学のイベントの手伝いを部活経由でしています．</p>
       </div>
