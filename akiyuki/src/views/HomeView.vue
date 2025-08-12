388d27f79c7ac2674f856411af826d6678247db4
llillillj <83072738+llillillj@users.noreply.github.com>
2025-08-12 21:39:20 +0900

    :art:update home-viewlayout as taking more spaced

--- akiyuki/src/views/HomeView.vue
+++ akiyuki/src/views/HomeView.vue
@@ -1,23 +1,26 @@
 <template>
   <q-page padding class="home flex flex-center">
     <div class="q-gutter-md flex flex-center">
-      <div class="text-content text-left q-pa-md q-gutter-y-md" style="overflow-y: initial;">
+      <div class="text-content text-left q-pa-md q-gutter-y-xl" style="overflow-y: initial;">
         <p class="text-h4">Akiyuki Koyama</p>
+        <q-separator color="grey-10" />
         <div>
           <p class="text-h5">所属</p>
           <li class="text-subtitle1"><a href="https://u-hyogo.info/" target="_blank" rel="noopener noreferrer">兵庫県立大学大学院 情報科学研究科 データ計算科学専攻</a></li>
           <li class="text-subtitle1"><a href="https://interaction-lab.org/ja/" target="_blank" rel="noopener noreferrer">川嶋研究室</a> 所属</li>
           <li class="text-subtitle1"><a href="https://www.dakken.co.jp/" target="_blank" rel="noopener noreferrer">合同会社DA研</a></li>
         </div>
+        <q-separator color="grey-10" />
         <div>
           <p class="text-h5">活動概要</p>
           <li class="text-subtitle1">専門：データ分析，機械学習，Webアプリ開発</li>
           <li class="text-subtitle1">スキル：Python，Vue.js，Git，Autodesk fusion，3Dプリンター，C#，Firestore</li>
           <li class="text-subtitle1">活動：機械学習コンペへの参加など</li>
         </div>
+        <q-separator color="grey-10" />
         <div>
           <p class="text-h5">過去の取り組み</p>
-           <table class="text-subtitle1">
+          <table class="text-subtitle1">
             <thead>
               <tr>
                 <th>年</th>
@@ -79,17 +82,20 @@
             </tbody>
           </table>
         </div>
+        <q-separator color="grey-10" />
         <div>
           <p class="text-h5">研究内容</p>
           <p class="text-subtitle1">研究室で取り組まれているプロジェクトの一つで<span style="color: orange;">魚群のインタラクション</span>について携わっています．卒業研究では，このテーマでの利用を想定した<span style="color: orange;">魚型ロボットの開発</span>を行いました．</p>
           <p class="text-subtitle1">学部卒業後は，同大学の情報科学研究科に進学し，魚型ロボットの改良，遊泳制御の<span style="color: orange;">モデル予測制御・強化学習</span>などによる最適化を用いて行っています．</p>
         </div>
+        <q-separator color="grey-10" />
         <div>
           <p class="text-h5">制作物</p>
           <li class="text-subtitle1">部活動の定例会資料自動作成ツール</li>
           <li class="text-subtitle1"><a href="https://llillillj.github.io/snake" target="_blank" rel="noopener noreferrer">Snake Game</a></li>
           <li class="text-subtitle1"><a href="https://llillillj.github.io/boid-algorithm/" target="_blank" rel="noopener noreferrer">Boid Algorithm</a></li>
         </div>
+        <q-separator color="grey-10" />
         <div>
           <p class="text-h5">リンク</p>
           <li class="text-subtitle1"><a href="https://www.kaggle.com/akiyukikouyama" target="_blank" rel="noopener noreferrer">Kaggle</a></li>
@@ -99,7 +105,7 @@
       </div>
     </div>
     <q-footer class="semi-transparent-bg bg-black">
-        <div class="text-caption text-center">© 2025 Akiyuki Kouyama / ad25u022@guh.u-hyogo.ac.jp</div>
+        <div class="text-caption text-center">© 2025 Akiyuki Koyama / ad25u022@guh.u-hyogo.ac.jp</div>
     </q-footer>
   </q-page>
 </template>
